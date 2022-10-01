c30752a08d1580e238f02329ef35c243d3134c37
Yasser Shkeir <shkeiryasser@gmail.com>
2022-10-01 16:09:30 +0300

    Structure: File Structure + Initial Outline

---
+++ dating_frontend/page_scripts.js
@@ -0,0 +1,45 @@
+const dating_pages = {};
+
+dating_pages.baseURL = "";
+
+dating_pages.Console = (title, values, oneValue = true) => {
+  console.log("---" + title + "---");
+  if (oneValue) {
+    console.log(values);
+  } else {
+    for (let i = 0; i < values.length; i++) {
+      console.log(values[i]);
+    }
+  }
+  console.log("--/" + title + "---");
+};
+
+dating_pages.getAPI = async (api_url) => {
+  try {
+    return await axios(api_url);
+  } catch (error) {
+    dating_pages.Console("Error from GET API", error);
+  }
+};
+
+dating_pages.postAPI = async (api_url, api_data, api_token = null) => {
+  try {
+    return await axios.post(api_url, api_data, {
+      headers: {
+        Authorization: "token " + api_token,
+      },
+    });
+  } catch (error) {
+    dating_pages.Console("Error from POST API", error);
+  }
+};
+
+dating_pages.loadFor = (page) => {
+  eval("dating_pages.load_" + page + "();");
+};
+
+dating_pages.load_landing = async () => {
+  const landing_url = `${dating_pages.baseURL}/landing`;
+  const response_landing = await dating_pages.getAPI(landing_url);
+  dating_pages.Console("Testing Landing API", response_landing.data.data);
+};
